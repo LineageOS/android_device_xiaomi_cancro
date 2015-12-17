55390f8eef5f1c7bdc43546c533971e1bc68a1e5
jrizzoli <joey@cyanogenmoditalia.it>
2015-12-17 21:16:35 +0100

    cancro: update FM for M
    Change-Id: I2777c5d3fa3b8abb83f812145dde5094875b7bb9
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- cancro.mk
+++ cancro.mk
@@ -290,10 +290,11 @@ PRODUCT_PACKAGES += \
 
 # FM Radio
 PRODUCT_PACKAGES += \
-	qcom.fmradio \
+    FMRadio \
+    libfmjni
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    hw.fm.internal_antenna=true
+    ro.fm.transmitter=false
 
 # USB
 PRODUCT_PACKAGES += \
