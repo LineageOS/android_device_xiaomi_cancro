ff08f601507e58e80decea8ad174864005a2f0bf
jrizzoli <joey@cyanogenmoditalia.it>
2016-02-06 21:41:20 +0100

    [DO NOT MERGE] cancro: get a gelling gello
    Change-Id: I6fd5a6b9248c77d5ef62f5797dd7a5d72a28c11d
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- cancro.mk
+++ cancro.mk
@@ -341,6 +341,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SoundRecorder
 
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
