1093789ad0e2f4d0ded3ce4122a527fc9f20cdf4
Dingjun <fangdingjun@gmail.com>
2016-04-11 15:20:49 +0800

    correct model name for MI 4 LTE
    Change-Id: Idd118828f38ffa3038dfee2eaeece0fec82cf403

--- init/init_cancro.cpp
+++ init/init_cancro.cpp
@@ -91,6 +91,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         case 1974:
             property_set("ro.product.model", "MI 4");
             break;
+        case 1972:
+            property_set("ro.product.model", "MI 4 LTE");
+            break;
         default:
             // Other unsupported variants
             property_set("ro.product.model", "Unsupported MI Cancro");
