e0537ab8b26545edb98131eda1eae75049fba336
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-10-06 16:11:20 +0200

    [DNM] cancro: camera: much haxx, so hal1
    Change-Id: I9d92bab7779b7648687bf42fe50109d3987f4e86
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,7 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
 
 # Camera
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
@@ -188,3 +189,4 @@ include device/qcom/sepolicy/sepolicy.mk
 #        $(CANCRO_PATH)/sepolicy
 
 -include vendor/xiaomi/cancro/BoardConfigVendor.mk
+#-include vendor/qcom/binaries/msm8974/graphics/BoardConfigVendor.mk
