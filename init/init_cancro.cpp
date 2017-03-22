6b8bb3d3028d1406a041185c372ff6aaba333bdd
Heiher <r@hev.cc>
2017-03-22 20:24:52 +0800

    cancro: fix default network for mi4 lte.
    Change-Id: I3f955927e14062a5d162508639dc0de4fbd7917a
    Signed-off-by: Heiher <r@hev.cc>

--- init/init_cancro.cpp
+++ init/init_cancro.cpp
@@ -115,9 +115,11 @@ void vendor_load_properties()
         case 1978:
             property_set("ro.product.model", "MI 3W");
             property_set("ro.nfc.port", "I2C");
+            property_set("ro.telephony.default_network", "9");
             break;
         case 1974:
             property_set("ro.product.model", "MI 4");
+            property_set("ro.telephony.default_network", "9");
             break;
         case 1972:
             property_set("ro.product.model", "MI 4LTE");
